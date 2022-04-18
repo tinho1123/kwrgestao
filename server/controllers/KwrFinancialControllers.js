63e851612ae068b1759ff34952520971f4758ed3
Wellington <tinhoefaela@gmail.com>
2022-04-18 17:59:28 -0300

    heroky impplements

--- server/controllers/KwrFinancialControllers.js
+++ server/controllers/KwrFinancialControllers.js
@@ -4,8 +4,8 @@ const getAll = async (_req, res) => {
     const result = await KwrFinancialServices.getAll();
     
     if (result instanceof Error) {
-        res.status(402).json({ message: result.message })
-    }
+        res.status(402).json({ message: result.message });
+    };
 
     res.status(200).json(result);
 }
